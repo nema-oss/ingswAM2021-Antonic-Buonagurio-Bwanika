d1c385dfe9d03508fdacb9e11be9c42120cfec45
Chiara <chiara.buonagurio@mail.polimi.it>
2021-05-28 11:58:48 +0200

    minor fixes

--- src/main/java/it/polimi/ingsw/view/client/gui/controllers/GameBoardController.java
+++ src/main/java/it/polimi/ingsw/view/client/gui/controllers/GameBoardController.java
@@ -35,6 +35,7 @@ public class GameBoardController implements Initializable{
         ClientCardMarket clientCardMarket = clientGameBoard.getCardMarket();
         ClientMarbleMarket clientMarbleMarket = clientGameBoard.getMarket();
 
+
         for(int i=0; i<3; i++)
             for(int j=0; j<4; j++) {
                 ImageView card = new ImageView(new Image("/gui/Images/DevelopmentCardsFront/" + clientCardMarket.getCard(i, j).getId() + ".png"));
@@ -45,7 +46,7 @@ public class GameBoardController implements Initializable{
 
                 card.setOnMouseClicked(this::buyDevelopmentCard);
 
-                ImageView marble = new ImageView(new Image("gui/Images/Marbles/WHITE.png" /* + clientMarbleMarket.getMarble(i,j).getColor().toString() +  ".png" */));
+                ImageView marble = new ImageView(new Image("gui/Images/Marbles/" + clientMarbleMarket.getMarble(i,j).getColor().toString() +  ".png" ));
                 marble.setPreserveRatio(true);
                 marble.setFitHeight(40);
                 marble.setFitWidth(40);

e4e740df6371ee5ae0159f354a6bab08faf31b59
Nema <nemanja.antonic99@gmail.com>
2021-07-02 11:50:45 +0200

    bug fix

--- src/main/java/it/polimi/ingsw/view/server/VirtualView.java
+++ src/main/java/it/polimi/ingsw/view/server/VirtualView.java
@@ -465,6 +465,7 @@ public class VirtualView implements VirtualViewInterface{
     public void activateProductionDevelopmentCard(String user, List<DevelopmentCard> developmentCards) {
 
         List<Error> errors = matchController.onActivateDevelopmentProduction(user, developmentCards);
+        System.out.println(errors);
         if (isActive) {
             if (errors.isEmpty())
                 onAcceptedActivateProductionDevelopmentCard(user, developmentCards);
